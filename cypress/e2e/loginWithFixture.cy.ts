b8c1ba68ece9ec2d63e4b921228db3a2a8ffd68b
Grace Wangare <gkungu65@gmail.com>
2025-07-07 22:04:18 +0200

    fixlogintests

--- cypress/e2e/loginWithFixture.cy.ts
+++ cypress/e2e/loginWithFixture.cy.ts
@@ -14,7 +14,7 @@ describe('Login', () => {
   
   it('Login with valid credentials', () => {
     cy.fixture('login').then((loginData) => {
-        loginDetails.getEmailSelector().type(loginData.valid);
+        loginDetails.getEmailSelector().type(loginData.validUsername);
         loginDetails.getPasswordSelector().type(loginData.validPassword);
         loginDetails.getLoginButtonSelector().click();
         loginDetails.getLoginAssertion().should('be.visible');
@@ -23,7 +23,7 @@ describe('Login', () => {
 
   it('Login with invalid password', () => {
     cy.fixture('login').then((loginData) => {
-        loginDetails.getEmailSelector().type(loginData.valid);
+        loginDetails.getEmailSelector().type(loginData.validUsername);
         loginDetails.getPasswordSelector().type(loginData.invalidPassword);
         loginDetails.getLoginButtonSelector().click();
         loginDetails.getErrorMessageSelector().should('be.visible');
@@ -36,6 +36,15 @@ describe('Login', () => {
         loginDetails.getPasswordSelector().type(loginData.validPassword);
         loginDetails.getLoginButtonSelector().click();
         loginDetails.getErrorMessageSelector().should('be.visible');
+    })
+  }) 
+  it.only('Login with empty passsword and empty email', () => {
+    cy.fixture('login').then((loginData) => {
+        loginDetails.getEmailSelector().type(' ');
+        loginDetails.getPasswordSelector().type(' ');
+        loginDetails.getLoginButtonSelector().click();
+        cy.contains('Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.').should('be.visible');
+        
     })
   }) 
  

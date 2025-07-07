af6ddf0679304d10d08ffc2d0a5f6ccb58bc7e19
Grace Wangare <gkungu65@gmail.com>
2025-07-07 21:28:50 +0200

    negativeLoginTests

--- cypress/e2e/loginWithFixture.cy.ts
+++ cypress/e2e/loginWithFixture.cy.ts
@@ -20,5 +20,23 @@ describe('Login', () => {
         loginDetails.getLoginAssertion().should('be.visible');
     })
   })  
+
+  it('Login with invalid password', () => {
+    cy.fixture('loginDetails').then((loginDetails) => {
+        loginDetails.getEmailSelector().type(loginDetails.valid);
+        loginDetails.getPasswordSelector().type(loginDetails.invalidPassword);
+        loginDetails.getLoginButtonSelector().click();
+        loginDetails.getErrorMessageSelector().should('be.visible');
+    })
+  })  
+
+   it('Login with invalid  username', () => {
+    cy.fixture('loginDetails').then((loginDetails) => {
+        loginDetails.getEmailSelector().type(loginDetails.invalidUsername);
+        loginDetails.getPasswordSelector().type(loginDetails.validPassword);
+        loginDetails.getLoginButtonSelector().click();
+        loginDetails.getErrorMessageSelector().should('be.visible');
+    })
+  }) 
  
  })    
